a40d1c241eea9c2ba8ab370691872c804af4beb2
Matt Conrad <mmconrad@gmail.com>
2017-03-10 11:25:12 +0100

    Add more to dictionary

--- utils/dictionary.js
+++ utils/dictionary.js
@@ -21,12 +21,60 @@ module.exports = {
     word: "kye",
     phonetic: "kuhyay"
   },
+  when: {
+    word: "kdaj",
+    phonetic: "kdye"
+  },
+  why: {
+    word: "zikaj",
+    phonetic: "zikye"
+  },
   ugly: {
     word: "grd",
     phonetic: "gird"
   },
+  best: {
+    word: "najboljši",
+    phonetic: "nye bowl she"
+  },
+  good: {
+    word: "dober",
+    phonetic: "dough burr"
+  },
+  better: {
+    word: "boljši",
+    phonetic: "bowl she"
+  },
+  bad: {
+    word: "slab",
+    phonetic: "slob"
+  },
+  left: {
+    word: "levo",
+    phonetic: "lay vo"
+  },
+  right: {
+    word: "desni",
+    phonetic: "days knee"
+  },
+  correct: {
+    word: "pravilen",
+    phonetic: "prow ill en"
+  },
   also: {
     word: "tudi",
     phonetic: "too dee"
+  },
+  "how are you": {
+    word: "kako si",
+    phonetic: "caw coe see"
+  },
+  "this": {
+    word: "to",
+    phonetic: "toe"
+  },
+  please: {
+    word: "prosim",
+    phonetic: "pro seem"
   }
 };
