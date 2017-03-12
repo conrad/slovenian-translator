1553ef911d1c09f23aa0ec69615b2b7fd6b6414a
Matt Conrad <mmconrad@gmail.com>
2017-03-12 10:26:10 +0100

    Add to dictionary

--- utils/dictionary.js
+++ utils/dictionary.js
@@ -1,6 +1,82 @@
 'use strict';
 
 module.exports = {
+  please: {
+    word: "prosim",
+    phonetic: "pro seem"
+  },
+  small: {
+    word: "mala",
+    phonetic: "malla"
+  },
+  little: {
+    word: "mala",
+    phonetic: "malla"
+  },
+  big: {
+    word: "velika",
+    phonetic: "vuh leeka"
+  },
+  large: {
+    word: "velika",
+    phonetic: "vuh leeka"
+  },
+  old: {
+    word: "star",
+    phonetic: "star"
+  },
+  new: {
+    word: "novo",
+    phonetic: "novo"
+  },
+  young: {
+    word: "mlad",
+    phonetic: "milad"
+  },
+  water: {
+    word: "voda",
+    phonetic: "voe duh"
+  },
+  red: {
+    word: "rdec",
+    phonetic: "ruh detch"
+  },
+  for: {
+    word: "za",
+    phonetic: "zah"
+  },
+  toward: {
+    word: "na",
+    phonetic: "nah"
+  },
+  and: {
+    word: "in",
+    phonetic: "een"
+  },
+  in: {
+    word: "na",
+    phonetic: "nah"
+  },
+  dog: {
+    word: "pes",
+    phonetic: "pace"
+  },
+  cat: {
+    word: "macka",
+    phonetic: "motch kuh"
+  },
+  bathroom: {
+    word: "kopalnica",
+    phonetic: "coe pall neat zuh"
+  },
+  mountain: {
+    word: "gora",
+    phonetic: "gora"
+  },
+  castle: {
+    word: "grad",
+    phonetic: "grod"
+  },
   beautiful: {
     word: "lepa",
     phonetic: "laypa"
@@ -34,7 +110,7 @@ module.exports = {
     phonetic: "gird"
   },
   best: {
-    word: "najboljši",
+    word: "najboljsi",
     phonetic: "nye bowl she"
   },
   good: {
@@ -42,7 +118,7 @@ module.exports = {
     phonetic: "dough burr"
   },
   better: {
-    word: "boljši",
+    word: "boljsi",
     phonetic: "bowl she"
   },
   bad: {
@@ -69,12 +145,8 @@ module.exports = {
     word: "kako si",
     phonetic: "caw coe see"
   },
-  "this": {
+  this: {
     word: "to",
     phonetic: "toe"
-  },
-  please: {
-    word: "prosim",
-    phonetic: "pro seem"
   }
 };
