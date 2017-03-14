c1f413e33b7d59b36ab7e1fe20bce4c782ea09db
Matt Conrad <mmconrad@gmail.com>
2017-03-14 10:49:17 +0100

    Add body parts to dictionary

--- utils/dictionary.js
+++ utils/dictionary.js
@@ -141,6 +141,210 @@ module.exports = {
     word: "tudi",
     phonetic: "too dee"
   },
+  recipe: {
+    word: "recept",
+    phonetic: "reh sept"
+  },
+  head: {
+    word: "glava",
+    phonetic: "glava"
+  },
+  tooth: {
+    word: "zob",
+    phonetic: "zobe"
+  },
+  teeth: {
+    word: "zobe",
+    phonetic: "zobay"
+  },
+  stomach: {
+    word: "trebuh",
+    phonetic: "tray boo"
+  },
+  back: {
+    word: "hrbet",
+    phonetic: "her bet"
+  },
+  throat: {
+    word: "grlo",
+    phonetic: "ger low"
+  },
+  neck: {
+    word: "vrat",
+    phonetic: "vrot"
+  },
+  eye: {
+    word: "oko",
+    phonetic: "oh coe"
+  },
+  eyes: {
+    word: "oci",
+    phonetic: "oh chi"
+  },
+  eyebrow: {
+    word: "obrv",
+    phonetic: "oh burr vuh"
+  },
+  arm: {
+    word: "roka",
+    phonetic: "row kah"
+  },
+  leg: {
+    word: "noga",
+    phonetic: "no gah"
+  },
+  arms: {
+    word: "roke",
+    phonetic: "row kay"
+  },
+  legs: {
+    word: "noge",
+    phonetic: "no gay"
+  },
+  shoulders: {
+    word: "rane",
+    phonetic: "rah nay"
+  },
+  knee: {
+    word: "koleno",
+    phonetic: "coe lay no"
+  },
+  butt: {
+    word: "zadnjica",
+    phonetic: "zod nyitsa"
+  },
+  ass: {
+    word: "rit",
+    phonetic: "reet"
+  },
+  finger: {
+    word: "prst",
+    phonetic: "pirst"
+  },
+  toe: {
+    word: "prst",
+    phonetic: "pirst"
+  },
+  fingers: {
+    word: "prsti",
+    phonetic: "pirsty"
+  },
+  toes: {
+    word: "prsti",
+    phonetic: "pirsty"
+  },
+  ear: {
+    word: "uho",
+    phonetic: "oohoe"
+  },
+  mouth: {
+    word: "usda",
+    phonetic: "oosdah"
+  },
+  lip: {
+    word: "usnica",
+    phonetic: "oose neatza"
+  },
+  lips: {
+    word: "usnice",
+    phonetic: "oose neatzay"
+  },
+  "my head hurts": {
+    word: "boli me glava",
+    phonetic: "bowley may glava"
+  },
+  "It's a pain in the ass": {
+    word: "boli me rit",
+    phonetic: "bowley may reet"
+  },
+  cheers: {
+    word: "za zdravje",
+    phonetic: "zah zdrahv yay"
+  },
+  "window": {
+    word: "okno",
+    phonetic: "oak no"
+  },
+  health: {
+    word: "zdravje",
+    phonetic: "zdrahv yay"
+  },
+  medecine: {
+    word: "zdravilo",
+    phonetic: "zdrahveelo"
+  },
+  eyelashes: {
+    word: "trepalnice",
+    phonetic: "tre pall neatzay"
+  },
+  hospital: {
+    word: "bolnica",
+    phonetic: "bowl neatza"
+  },
+  pharmacy: {
+    word: "lekarna",
+    phonetic: "lekarna"
+  },
+  "I need": {
+    word: "potrebujem",
+    phonetic: "poe tray boo yem"
+  },
+  "I want": {
+    word: "potrebujem",
+    phonetic: "poe tray boo yem"
+  },
+  her: {
+    word: "njo",
+    phonetic: "niyo"
+  },
+  "I miss her": {
+    word: "jaz pogresam njo",
+    phonetic: "yahz poe gressam niyo"
+  },
+  "I'm healthy": {
+    word: "sem zdrav",
+    phonetic: "sem zdrahv"
+  },
+  hot: {
+    word: "vroce",
+    phonetic: "vroachay"
+  },
+  cold: {
+    word: "hlad",
+    phonetic: "hlod"
+  },
+  on: {
+    word: "na",
+    phonetic: "nah"
+  },
+  for: {
+    word: "za",
+    phonetic: "zah"
+  },
+  from: {
+    word: "iz",
+    phonetic: "ease"
+  },
+  under: {
+    word: "pod",
+    phonetic: "poad"
+  },
+  beside: {
+    word: "poleg",
+    phonetic: "poe layg"
+  },
+  "what's up": {
+    word: "kaj dogaja",
+    phonetic: "kye doag eye ya"
+  },
+  "one sec": {
+    word: "samo malo",
+    phonetic: "saw mow mall owe"
+  },
+  wait: {
+    word: "pocakaj",
+    phonetic: "poe cha kye"
+  },
   "how are you": {
     word: "kako si",
     phonetic: "caw coe see"
